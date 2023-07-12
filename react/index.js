4cf4efbd7e29551734bed2263cb4f853ea08cf36
Sagar <22395605+sagar2git@users.noreply.github.com>
2023-07-13 01:04:51 +0545

    fun facts about react

--- react/index.js
+++ react/index.js
@@ -1,2 +1,17 @@
+const page = 
+	<div>
+	  <img src="./logo.svg" width="70px" />
+		<h1>Fun facts about React</h1>
+		<ul>
+			<li>Was first released in 2013</li>
+			<li>Was originally created by Jordan Walker</li>
+			<li>Has well over 100K stars on GitHub</li>
+			<li>Is maintained by Facebook</li>
+			<li>Powers thousands of enterprise apps, including mobile apps</li>
+		</ul>
+	</div>
 
-ReactDOM.render(<p>Hello world</p>, document.getElementById("root"));
+ReactDOM.render(
+  page,
+  document.getElementById("root")
+  )
